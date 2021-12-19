4981e1e694393df1af1925af8899f1fb14bbaf50
taoliujun <taoliujun@qq.com>
2021-12-19 14:33:48 +0800

    chore: add package scripts

--- .babelrc.js
+++ .babelrc.js
@@ -7,7 +7,7 @@ module.exports = {
                 corejs: 3,
             },
         ],
-        '@babel/preset-typescript'
+        '@babel/preset-typescript',
     ],
     sourceType: 'unambiguous',
 };
