11f1761116c57f19fa702f729e95222dbfd1a42a
taoliujun <taoliujun@qq.com>
2023-12-08 16:06:10 +0800

    chore: script

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,18 +1,25 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const defaultPlugin = require('@commitlint/config-conventional');
 
+const getRules = (input) => {
+  if (typeof input === 'string') {
+    return [input];
+  }
+  return input;
+};
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
       defaultPlugin.rules['type-enum'][0],
       defaultPlugin.rules['type-enum'][1],
-      [...defaultPlugin.rules['type-enum'][2], ...'RELEASING'],
+      [...getRules(defaultPlugin.rules['type-enum'][2]), 'RELEASING'],
     ],
     'type-case': [
       defaultPlugin.rules['type-case'][0],
       defaultPlugin.rules['type-case'][1],
-      [...defaultPlugin.rules['type-case'][2], 'upper-case'],
+      [...getRules(defaultPlugin.rules['type-case'][2]), 'upper-case'],
     ],
   },
 };
