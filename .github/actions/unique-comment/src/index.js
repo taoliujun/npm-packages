046b4a55fb1c967690a6ddb5d4e1753ac7014fd0
taoliujun <taoliujun@qq.com>
2023-12-27 16:05:00 +0800

    fix: update

--- .github/actions/unique-comment/src/index.js
+++ .github/actions/unique-comment/src/index.js
@@ -1,37 +1,43 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-let octokit;
-
-const listComments = async () => {};
-
-const createComment = async () => {};
-
-const updateComment = async () => {};
-
-const getUniqueComment = async () => {};
-
 const main = async () => {
     const token = core.getInput('token');
     const uniqueIdentifier = `[^uniqueIdentifier] ${core.getInput('uniqueIdentifier')}`;
-    const body = core.getInput('body');
+    const body = `${core.getInput('body')}\n\n---\n\n${uniqueIdentifier}`;
 
     const { owner, repo } = github.context.repo;
     const issueNumber = github.context.issue.number;
 
-    console.log(
-        '==main',
-        JSON.stringify({
-            token,
-            uniqueIdentifier,
-            body,
+    const octokit = github.getOctokit(token);
+
+    const comments = await octokit.rest.issues.listComments({
+        owner,
+        repo,
+        issue_number: issueNumber,
+    });
+
+    const botComment = comments.data.find((v) => v.body.includes(uniqueIdentifier));
+
+    if (botComment) {
+        await octokit.rest.issues.updateComment({
             owner,
             repo,
-            issueNumber,
-        }),
-    );
-
-    octokit = github.getOctokit(token);
+            comment_id: botComment.id,
+            body,
+        });
+    } else {
+        await octokit.rest.issues.createComment({
+            owner,
+            repo,
+            issue_number: issueNumber,
+            body,
+        });
+    }
 };
 
-main();
+try {
+    main();
+} catch (err) {
+    core.setFailed(err.message);
+}
