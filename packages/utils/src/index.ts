831a6f24c3c33362561eb974c3127fa6e67ff3fd
taoliujun <taoliujun@qq.com>
2023-12-29 17:58:40 +0800

    fix: update

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,2 +1,13 @@
 export * from './money';
-export * from './ahooks';
+export * from './ahooks';  
+
+interface AJ {
+    name: string;
+    age: number;
+}
+
+const a: AJ = {
+    name: 'hello',
+};
+
+console.log(a);
