7422777016d85d824dfedd97d4e62752e751b352
taoliujun <taoliujun@qq.com>
2024-01-04 10:40:09 +0800

    docs(changeset): chore: adjust umd build workflow

--- packages/json-beautiful-render/config/webpack.umd.js
+++ packages/json-beautiful-render/config/webpack.umd.js
@@ -1,7 +1,7 @@
 module.exports = {
     mode: 'production',
     target: 'web',
-    entry: './lib/esm/index.js',
+    entry: './lib/umd/_tmp/index.js',
     output: {
         filename: 'index.js',
         path: `${__dirname}/../lib/umd`,
