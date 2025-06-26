2e8b40e388164680815a50dc9e1e28e3309e55c8
taoliujun <taoliujun@qq.com>
2025-06-26 11:14:39 +0800

    fix: update

--- packages/common-utils/src/utils/httpHelper.ts
+++ packages/common-utils/src/utils/httpHelper.ts
@@ -1,16 +1,8 @@
-import { ProxyAgent } from 'undici';
 import { jsonParse, jsonStringify } from './json';
 
 /** 发起http请求 */
 export class HttpHelper {
-    public async baseRequest<T>(
-        url: string,
-        params: RequestInit,
-        opts?: Partial<{
-            /** 代理 */
-            httpProxy: string;
-        }>,
-    ) {
+    public async baseRequest<T>(url: string, params: RequestInit) {
         const signal = new AbortController();
 
         const t = setTimeout(() => {
@@ -23,9 +15,6 @@ export class HttpHelper {
                 ...params?.headers,
             },
             signal: signal.signal,
-            ...(opts?.httpProxy && {
-                dispatcher: new ProxyAgent(opts.httpProxy),
-            }),
         });
 
         clearTimeout(t);
@@ -35,126 +24,70 @@ export class HttpHelper {
         });
     }
 
-    async post<T>(
-        url: string,
-        data: unknown,
-        headers?: Record<string, string>,
-        opts?: Partial<{
-            /** 代理 */
-            httpProxy: string;
-        }>,
-    ) {
-        return this.baseRequest<T>(
-            url,
-            {
-                method: 'POST',
-                body: jsonStringify(data),
-                headers: {
-                    'Content-Type': 'application/json',
-                    ...headers,
-                },
+    async post<T>(url: string, data: unknown, headers?: Record<string, string>) {
+        return this.baseRequest<T>(url, {
+            method: 'POST',
+            body: jsonStringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+                ...headers,
             },
-            opts,
-        );
+        });
     }
 
-    async postWithFormData<T>(
-        url: string,
-        data: object,
-        headers?: Record<string, string>,
-        opts?: Partial<{
-            /** 代理 */
-            httpProxy: string;
-        }>,
-    ) {
+    async postWithFormData<T>(url: string, data: object, headers?: Record<string, string>) {
         const body = new FormData();
         Object.entries(data).forEach(([key, value]) => {
             body.append(key, value);
         });
 
-        return this.baseRequest<T>(
-            url,
-            {
-                method: 'POST',
-                body,
-                headers: {
-                    ...headers,
-                },
+        return this.baseRequest<T>(url, {
+            method: 'POST',
+            body,
+            headers: {
+                ...headers,
             },
-            opts,
-        );
+        });
     }
 
     async get<T>(
         url: string,
         data?: ConstructorParameters<typeof URLSearchParams>[0],
         headers?: Record<string, string>,
-        opts?: Partial<{
-            /** 代理 */
-            httpProxy: string;
-        }>,
     ) {
         const q = new URLSearchParams(data).toString();
         const queryFlag = url.includes('?') ? '&' : '?';
 
         const sUrl = `${url}${q ? queryFlag : ''}${q}`;
 
-        return this.baseRequest<T>(
-            sUrl,
-            {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    ...headers,
-                },
+        return this.baseRequest<T>(sUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                ...headers,
             },
-            opts,
-        );
+        });
     }
 
-    async put<T>(
-        url: string,
-        data: unknown,
-        headers?: Record<string, string>,
-        opts?: Partial<{
-            /** 代理 */
-            httpProxy: string;
-        }>,
-    ) {
-        return this.baseRequest<T>(
-            url,
-            {
-                method: 'PUT',
-                body: jsonStringify(data),
-                headers: {
-                    'Content-Type': 'application/json',
-                    ...headers,
-                },
+    async put<T>(url: string, data: unknown, headers?: Record<string, string>) {
+        return this.baseRequest<T>(url, {
+            method: 'PUT',
+            body: jsonStringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+                ...headers,
             },
-            opts,
-        );
+        });
     }
 
-    async delete<T>(
-        url: string,
-        data: unknown,
-        headers?: Record<string, string>,
-        opts?: Partial<{
-            /** 代理 */
-            httpProxy: string;
-        }>,
-    ) {
-        return this.baseRequest<T>(
-            url,
-            {
-                method: 'GET',
-                body: jsonStringify(data),
-                headers: {
-                    'Content-Type': 'application/json',
-                    ...headers,
-                },
+    async delete<T>(url: string, data: unknown, headers?: Record<string, string>) {
+        return this.baseRequest<T>(url, {
+            method: 'GET',
+            body: jsonStringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+                ...headers,
             },
-            opts,
-        );
+        });
     }
 }
