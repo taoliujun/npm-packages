714cd7092e182bbaaf3ed232f86258eb605544da
taoliujun <taoliujun@qq.com>
2025-07-30 22:50:44 +0800

    fix: update

--- packages/common-utils/src/utils/json.ts
+++ packages/common-utils/src/utils/json.ts
@@ -1,11 +1,26 @@
 import BIGJSON from 'json-bigint';
 
-/** json parse 支持大数字 */
+/** json parse 支持大数字
+ * @deprecated use jsonParse2
+ */
 export const jsonParse = <T>(text: string) => {
     try {
         return BIGJSON.parse(text) as T;
     } catch {
         return null;
+    }
+};
+
+/** json parse 支持大数字 */
+export const jsonParse2 = <T>(text: string | null | undefined, defaultValue: unknown = {}): T => {
+    if (!text) {
+        return defaultValue as T;
+    }
+
+    try {
+        return BIGJSON.parse(text) as T;
+    } catch {
+        return defaultValue as T;
     }
 };
 
